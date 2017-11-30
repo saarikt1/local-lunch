051c5db5c9400b07165856df7d935ad185ffb338
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2017-11-30 11:06:11 +0200

    Add more fields to restaurant-details

--- src/js/components/restaurant_detail.js
+++ src/js/components/restaurant_detail.js
@@ -4,17 +4,41 @@ class RestaurantDetail extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { name: ''}
+    this.state = { 
+      name: 'Factory aka Lihamonttu',
+      description: '',
+      address: ''
+    }
   }
 
   render() {
     return (
       <div className="details-form">
-        <h1>This is the details view!</h1>
-        <input
-          className="restaurant-name"
-          value={this.state.name}
-          onChange={event => this.setState({name: event.target.value})} />
+        <h1>{this.state.name}</h1>
+        
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={event => this.setState({name: event.target.value})} />
+        </label>
+
+        <label>
+          Description:
+          <textarea
+            type="text"
+            value={this.state.description}
+            onChange={event => this.setState({description: event.target.value})} />
+        </label>
+
+        <label>
+          Address:
+          <input
+            type="text"
+            value={this.state.address}
+            onChange={event => this.setState({address: event.target.value})} />
+        </label>
       </div>
     )
   }
