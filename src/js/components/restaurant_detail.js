016b411a45d6daea52f2ac8798be8a2ffd7559d1
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2017-11-30 16:17:50 +0200

    Clean up the forms

--- src/js/components/restaurant_detail.js
+++ src/js/components/restaurant_detail.js
@@ -15,30 +15,33 @@ class RestaurantDetail extends Component {
     return (
       <div className="details-form">
         <h1>{this.state.name}</h1>
-      <form className="pure-form pure-form-stacked">
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.name}
-            onChange={event => this.setState({name: event.target.value})} />
-        </label>
+        <form className="pure-form pure-form-stacked">
+          <legend>Update restaurant information</legend>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={event => this.setState({name: event.target.value})} />
+           </label>
 
-        <label>
-          Description:
-          <textarea
-            type="text"
-            value={this.state.description}
-            onChange={event => this.setState({description: event.target.value})} />
-        </label>
+            <label>
+              Description:
+              <textarea
+                type="text"
+                value={this.state.description}
+                onChange={event => this.setState({description: event.target.value})} />
+            </label>
 
-        <label>
-          Address:
-          <input
-            type="text"
-            value={this.state.address}
-            onChange={event => this.setState({address: event.target.value})} />
-        </label>
+            <label>
+              Address:
+              <input
+                type="text"
+                value={this.state.address}
+                onChange={event => this.setState({address: event.target.value})} />
+            </label>
+
+            <button type="submit" className="pure-button pure-button-primary">Save</button>
         </form>
       </div>
     )
