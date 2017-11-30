35e769ef46d127d6c0b29f5646702bfdfffc96e7
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2017-11-30 10:41:45 +0200

    Add input field to restaurant_detail

--- src/js/components/restaurant_detail.js
+++ src/js/components/restaurant_detail.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react'
 class RestaurantDetail extends Component {
   constructor(props) {
     super(props)
+
+    this.state = { name: ''}
   }
 
   render() {
     return (
       <div className="details-form">
         <h1>This is the details view!</h1>
+        <input
+          className="restaurant-name"
+          value={this.state.name}
+          onChange={event => this.setState({name: event.target.value})} />
       </div>
     )
   }
