98ab803509d10b39e4f8368f421633ce30598578
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-06-24 10:45:05 +0200

    Render restaurant info on landing page

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    initRestaurants();
+  }, []);
+
+  const initRestaurants = async () => {
+    const response = await axios.get("/restaurants");
+    setRestaurants(response.data);
+  };
+
   return (
     <div>
-      <h1>This is where you should go for lunch now:</h1>
+      {/* {console.log(restaurants)} */}
+      <h1>This is where you should go for lunch now</h1>
+      {restaurants.map((r) => (
+        <div key={r.id}>
+          <h3>{r.name}</h3>
+          <p>{r.address}</p>
+          <p>{r.web_page}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
