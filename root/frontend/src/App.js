d99fa0b2d354e7f9ac1f3c2936e6002df7a17628
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-06-24 14:57:20 +0200

    Calculate distance to restaurants from hardcoded value

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -21,10 +21,12 @@ const App = () => {
       const long = position.coords.longitude;
 
       status.textContent = `Your position: ${lat}, ${long}`;
+      return position;
     }
 
     function error() {
       status.textContent = "Unable to retrieve your location";
+      return null;
     }
 
     if (!navigator.geolocation) {
@@ -35,22 +37,54 @@ const App = () => {
     }
   }
 
+  function calculateDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
+    var earthRadius = 6371;
+    var dLat = deg2rad(lat2 - lat1);
+    var dLon = deg2rad(lon2 - lon1);
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var distance = earthRadius * c;
+    return distance;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+
   return (
     <div>
       {/* {console.log(restaurants)} */}
       <h1>This is where you should go for lunch now</h1>
+      {/* <button id="find-me" onClick={geoFindMe}>
+        Show my location
+      </button> */}
+      <p id="status"></p>
       {restaurants.map((r) => (
         <div key={r.id}>
           <h3>{r.name}</h3>
           <p>{r.address}</p>
           <p>{r.web_page}</p>
+          <p>
+            {r.latlon.x}, {r.latlon.y}
+          </p>
+          <p>
+            Distance:&nbsp;
+            {calculateDistanceBetweenPoints(
+              48.1979175,
+              16.3692964,
+              r.latlon.x,
+              r.latlon.y
+            )}
+            &nbsp;km
+          </p>
+          {geoFindMe()}
         </div>
       ))}
-      <button id="find-me" onClick={geoFindMe}>
-        Show my location
-      </button>
-      <br />
-      <p id="status"></p>
     </div>
   );
 };
