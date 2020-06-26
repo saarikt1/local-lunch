c9b152f03d224a92e293f6ba1ea0c6fd1fc3c3a8
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-06-26 10:27:41 +0200

    Refactor sorting function

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -6,7 +6,7 @@ import Map from "./components/Map";
 
 const App = () => {
   const [restaurants, setRestaurants] = useState([]);
-  const [userLocation, setUserLocation] = useState({ lat: null, lon: null });
+  const [userLocation, setUserLocation] = useState(null);
   const [isSorted, setIsSorted] = useState(false);
 
   useEffect(() => {
@@ -14,31 +14,24 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log("useEffect triggers");
-    const addDistanceToRestaurants = () => {
-      console.log("Adding distance to restaurants");
-      const restaurantsWithDistances = restaurants.map((r) => {
-        r.distance = calculateDistanceBetweenPoints(
-          userLocation.lat,
-          userLocation.lon,
-          r.latlon.x,
-          r.latlon.y
-        );
-        return r;
-      });
-
-      setRestaurants(restaurantsWithDistances);
-    };
-
     const sortRestaurantsByDistance = () => {
-      console.log("Sorting restaurants...");
-      setRestaurants([...restaurants].sort((a, b) => a.distance - b.distance));
+      const sortedRestaurants = restaurants
+        .map((r) => {
+          r.distance = calculateDistanceBetweenPoints(
+            userLocation.lat,
+            userLocation.lon,
+            r.latlon.x,
+            r.latlon.y
+          );
+          return r;
+        })
+        .sort((a, b) => a.distance - b.distance);
+
+      setRestaurants(sortedRestaurants);
       setIsSorted(true);
     };
 
-    if (userLocation.lat && !isSorted) {
-      console.log("User location: ", userLocation);
-      addDistanceToRestaurants();
+    if (userLocation && !isSorted) {
       sortRestaurantsByDistance();
     }
   }, [restaurants, userLocation, isSorted]);
@@ -58,7 +51,6 @@ const App = () => {
         lat: position.coords.latitude,
         lon: position.coords.longitude,
       });
-      // setIsUserLocated(true);
     }
 
     function error() {
