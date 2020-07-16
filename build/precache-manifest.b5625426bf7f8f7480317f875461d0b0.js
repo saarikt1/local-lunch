3eb163f7dfae8060e8753f0c0708db48b4ea87e4
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-07-16 10:40:56 +0200

    Add frontend build

---
+++ build/precache-manifest.b5625426bf7f8f7480317f875461d0b0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c185e4804728768e3af60df79394d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251c6f7948a95292f5bb",
+    "url": "/static/css/2.d557930c.chunk.css"
+  },
+  {
+    "revision": "251c6f7948a95292f5bb",
+    "url": "/static/js/2.f7e7e7a9.chunk.js"
+  },
+  {
+    "revision": "23ea44c127b59f493fbe2118649f6d74",
+    "url": "/static/js/2.f7e7e7a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2401f851b4d8a8dfe27e",
+    "url": "/static/js/main.3e6df018.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/static/media/roboto-latin-300-normal.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/static/media/roboto-latin-300-normal.ef7c6637.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/static/media/roboto-latin-400-normal.60fa3c06.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/static/media/roboto-latin-500-normal.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/static/media/roboto-latin-500-normal.87284894.woff"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/static/media/roboto-latin-700-normal.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/static/media/roboto-latin-700-normal.adcde98f.woff"
+  }
+]);
