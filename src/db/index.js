144037f0a24211fca514ef1d7622756cd4975743
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-07-16 09:51:29 +0200

    Change pg config

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,34 @@
 import dotenv from "dotenv";
-import pg from "pg";
+import { client } from "pg";
 
 dotenv.config();
 
-const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT } = process.env;
-const { Pool } = pg;
-
-const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_DATABASE,
-  password: DB_PASSWORD,
-  port: DB_PORT,
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
+client.connect();
+
+// const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT } = process.env;
+// const { Pool } = pg;
+
+// console.log("DATABASE_URL: ", process.env.DATABASE_URL);
+
+// const pool = new Pool({
+//   user: DB_USER,
+//   host: DB_HOST,
+//   database: DB_DATABASE,
+//   password: DB_PASSWORD,
+//   port: DB_PORT,
+// });
+
+// export default {
+//   query: (text, params) => pool.query(text, params),
+// };
+
 export default {
-  query: (text, params) => pool.query(text, params),
+  query: (text, params) => client.query(text, params),
 };
