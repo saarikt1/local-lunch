1c76cc2d58e0b029c49df2cb7be41965cbbf4eb5
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-07-17 16:48:57 +0200

    Install Morgan for logging

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import router from "./routes/routes.js";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const { PORT } = process.env;
 
 server.use(express.json());
 server.use(cors());
+server.use(morgan("dev"));
 server.use("/", router);
 server.use(express.static("build"));
 
