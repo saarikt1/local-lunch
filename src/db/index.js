2fcc9a40668499014821c308db77e7842264e9a1
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-07-17 10:27:18 +0200

    Cleanup after debugging

--- src/db/index.js
+++ src/db/index.js
@@ -3,30 +3,12 @@ import pg from "pg";
 
 dotenv.config();
 
-// const { Client } = pg;
-
-// console.log(process.env.DATABASE_URL);
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// client.connect();
-
-// export default {
-//   query: (text, params) => client.query(text, params),
-// };
-
 const { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT } = process.env;
 
 const { Pool } = pg;
 let pool;
 
 if (process.env.NODE_ENV === "development") {
-  console.log("We are in development mode right now!");
-
   pool = new Pool({
     user: DB_USER,
     host: DB_HOST,
@@ -35,8 +17,6 @@ if (process.env.NODE_ENV === "development") {
     port: DB_PORT,
   });
 } else {
-  console.log("We are in production mode right now!", process.env.DATABASE_URL);
-
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
