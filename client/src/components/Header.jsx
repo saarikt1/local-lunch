91f9ee260a2492fe4c866f30f40f5cc92015b36c
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-09-15 17:11:38 +0200

    Clean up Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 
-const Header = () => {
-  return (
-    <div>
-      <Typography id="page-header" variant="h4" align="center" gutterBottom>
-        Lunch Near Me
-      </Typography>
-    </div>
-  );
-};
+const Header = () => (
+  <div>
+    <Typography id="page-header" variant="h4" align="center" gutterBottom>
+      Lunch Near Me
+    </Typography>
+  </div>
+);
 
 export default Header;
