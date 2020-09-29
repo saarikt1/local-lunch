c0a557f5b8125551574cdd0f32d2feb60d1ac3b0
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-09-29 14:30:49 +0200

    Refactor getLocation

--- client/src/redux/location.js
+++ client/src/redux/location.js
@@ -1,7 +1,8 @@
 import { showNotification } from "./notification";
 
-const REQUEST_LOCATION = "[location] Geolocation request";
-const SET_LOCATION = "[location] Set location";
+const FETCH_LOCATION_REQUEST = "[location] Fetch request";
+const FETCH_LOCATION_SUCCESS = "[location] Fetch success";
+const FETCH_LOCATION_FAILURE = "[location] Fetch failure";
 
 const initialState = {
   coordinates: null,
@@ -11,39 +12,33 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case REQUEST_LOCATION:
+    case FETCH_LOCATION_REQUEST:
       return {
         ...state,
         isLocating: true,
         didInvalidate: false,
       };
-    case SET_LOCATION:
+    case FETCH_LOCATION_SUCCESS:
       return {
         ...state,
         isLocating: false,
         didInvalidate: false,
         coordinates: action.coords,
       };
+    case FETCH_LOCATION_FAILURE:
+      return {
+        ...state,
+        isLocating: false,
+        didInvalidate: false,
+      };
     default:
       return state;
   }
 };
 
-const requestLocation = () => {
-  return {
-    type: REQUEST_LOCATION,
-  };
-};
-
-const getCurrentPosition = (options = {}) => {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options);
-  });
-};
-
 const setLocation = (position) => {
   return {
-    type: SET_LOCATION,
+    type: FETCH_LOCATION_SUCCESS,
     coords: {
       lat: position.coords.latitude,
       lon: position.coords.longitude,
@@ -51,19 +46,30 @@ const setLocation = (position) => {
   };
 };
 
+const getPositionPromise = () => {
+  const options = {
+    enableHighAccuracy: false,
+    maximumAge: 30000,
+    timeout: 5000,
+  };
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  });
+};
+
 export const locateUser = () => async (dispatch) => {
-  if (!navigator.geolocation) {
-    dispatch(
-      showNotification(
-        "Geolocation is not supported by your browser",
-        "warning"
-      )
-    );
-    throw new Error("Geolocation not supported");
-  }
-  dispatch(requestLocation());
+  dispatch({ type: FETCH_LOCATION_REQUEST });
   try {
-    const position = await getCurrentPosition();
+    if (!navigator.geolocation) {
+      dispatch(
+        showNotification(
+          "Geolocation is not supported by your browser",
+          "warning"
+        )
+      );
+      throw new Error("Geolocation not supported");
+    }
+    const position = await getPositionPromise();
     if (position.coords.accuracy > 1000) {
       dispatch(
         showNotification(
@@ -77,6 +83,10 @@ export const locateUser = () => async (dispatch) => {
     }
     return position;
   } catch (err) {
+    dispatch({ type: FETCH_LOCATION_FAILURE });
+    if (err.code === 2) {
+      throw new Error("Location unavailable");
+    }
     throw err;
   }
 };
