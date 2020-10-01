0224f6e75747a237bd024d0699d1eae9ba8d8ad9
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-10-01 17:30:20 +0200

    Remove Enzyme

--- client/src/view/components/Header.test.js
+++ client/src/view/components/Header.test.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
 import Header from "./Header";
 
 describe("<Header />", () => {
-  it("renders without crashing", () => {
-    shallow(<Header />);
-  });
-
-  it("renders page header", () => {
-    const wrapper = shallow(<Header />);
-
-    expect(wrapper.text().includes("Lunch Near Me")).toBe(true);
+  it("renders header text", () => {
+    const { getByText } = render(<Header />);
+    expect(getByText("Lunch Near Me")).toBeInTheDocument();
   });
 });
