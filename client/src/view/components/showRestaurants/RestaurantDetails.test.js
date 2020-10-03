bdd8e4ef84cb238c40a7e026c3bbfa64bf804130
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-10-03 11:32:31 +0200

    Finish RestaurantDetails test

--- client/src/view/components/showRestaurants/RestaurantDetails.test.js
+++ client/src/view/components/showRestaurants/RestaurantDetails.test.js
@@ -1,24 +1,44 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import RestaurantDetails from "./RestaurantDetails";
 
-test("renders content", () => {
-  const restaurant = {
-    id: "41",
-    name: "Taikakattila",
-    website: "http://www.taikakattila.fi/",
-    latlon: {
-      x: 60.2923294,
-      y: 24.988834,
-    },
-    subtitle: "",
-    distance: 12620,
-  };
+describe("<RestaurantDetails />", () => {
+  test("renders restaurant name and website link", () => {
+    const restaurant = {
+      id: "41",
+      name: "Taikakattila",
+      website: "http://www.taikakattila.fi/",
+      latlon: {
+        x: 60.2923294,
+        y: 24.988834,
+      },
+      subtitle: "",
+      distance: 12620,
+    };
 
-  const component = render(<RestaurantDetails restaurant={restaurant} />);
+    const component = render(<RestaurantDetails restaurant={restaurant} />);
 
-  expect(component.container).toHaveTextContent("Taikakattila");
-  expect(component.container).toHaveTextContent("Website");
-  // expect(component.container).toHaveTextContent("http://www.taikakattila.fi/");
+    expect(screen.getByText(/Taikakattila/)).toBeInTheDocument();
+    expect(screen.getByText(/website/i)).toBeInTheDocument();
+  });
+
+  test("doesn't render website link if no website", () => {
+    const restaurant = {
+      id: "41",
+      name: "Taikakattila",
+      website: null,
+      latlon: {
+        x: 60.2923294,
+        y: 24.988834,
+      },
+      subtitle: "",
+      distance: 12620,
+    };
+
+    const component = render(<RestaurantDetails restaurant={restaurant} />);
+
+    expect(screen.getByText(/Taikakattila/)).toBeInTheDocument();
+    expect(screen.queryByText(/website/i)).toBeNull();
+  });
 });
